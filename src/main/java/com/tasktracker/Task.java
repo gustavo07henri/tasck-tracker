ccf14239921716ada76730e4250f7dc2db0b2bb5
Gustavo <gustavohe2016@gmail.com>
2024-11-19 16:55:14 -0300

    first commit

---
+++ src/main/java/com/tasktracker/Task.java
@@ -0,0 +1,47 @@
+package com.tasktracker;
+
+import java.time.LocalDateTime;
+
+public class Task {
+    private int id;
+    private String description;
+    private String status;
+    private LocalDateTime createdAT;
+    private LocalDateTime updatedAT;
+
+    public Task(int id, String description, String status, LocalDateTime createdAT, LocalDateTime updatedAT){
+        this.id = id;
+        this.description = description;
+        this.status = status;
+        this.createdAT = createdAT;
+        this.updatedAT = updatedAT;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+    public int getId(){
+        return id;
+    }
+    public void setDescription(String description){
+        this.description = description;
+    }
+    public String getDescription(){
+        return description;
+    }
+    public void setStatus(String status){
+        this.status = status;
+    }
+    public String getStatus(){
+        return status;
+    }
+    public LocalDateTime getCreatedAT() {
+        return createdAT;
+    }
+    public void setUpdatedAT(LocalDateTime updatedAT) {
+        this.updatedAT = updatedAT;
+    }
+    public LocalDateTime getUpdatedAT() {
+        return updatedAT;
+    }
+}
